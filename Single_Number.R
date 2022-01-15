34e94e2773fa2a01db0925fd56ca754752dd7de0
lauramcquaid <laura.mcquaid@ul.ie>
2022-01-15 20:36:01 +0000

    Single number code.

--- Single_Number.R
+++ Single_Number.R
@@ -1,5 +1,4 @@
 #Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
-#You must implement a solution with a linear runtime complexity and use only constant extra space.
 
 single_number <- function(x){
   as.vector(which(table(x)!=2))
